3ed92f12a553b032d91495bfef8c75a3c1af726b
598403 <matthewforjan@c02t50ljh03q.users.bah.com>
2018-07-16 14:54:01 -0400

    created separate store file

--- src/index.js
+++ src/index.js
@@ -2,37 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import Main from './components/Main';
-import data from './data/data';
-import rootReducer from './reducers/index';
-
-const initialState = {
-  timeReport: data.rows,
-  messages: data.messages,
-  absences: data.absences,
-  dialogs: {
-    drawerOpen: false,
-    timeDialog: {
-      open: false,
-      checked: false,
-    },
-    messagesDialogOpen: false,
-    absencesDialog: {
-      open: false,
-      startDate: '',
-      endDate: '',
-      absenceReason: '',
-      travelReason: '',
-    },
-    infoOpen: false,
-    snackbarOpen: false,
-  }
-}
-
-const store = createStore(rootReducer, initialState)
+import store from './store';
 
 ReactDOM.render(<Provider store={store}>
   <Main />
---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { createStore } from 'redux';
+import data from './data/data';
+import rootReducer from './reducers/index';
+
+const initialState = {
+  timeReport: data.rows,
+  messages: data.messages,
+  absences: data.absences,
+  dialogs: {
+    drawerOpen: false,
+    timeDialog: {
+      open: false,
+      checked: false,
+    },
+    messagesDialogOpen: false,
+    absencesDialog: {
+      open: false,
+      startDate: '',
+      endDate: '',
+      absenceReason: '',
+      travelReason: '',
+    },
+    infoOpen: false,
+    snackbarOpen: false,
+  }
+}
+
+const store = createStore(rootReducer, initialState)
+
+export default store;
