f049c6925403d34cf843ec0dee508fe4c0d181ce
598403 <matthewforjan@c02t50ljh03q.users.bah.com>
2018-07-26 14:10:24 -0400

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from './components/Main';
+import store from './store';
+import { Provider } from 'react-redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}>
+    <Main />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
