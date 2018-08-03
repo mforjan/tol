01a01b00cf216d3c87323cacb108cb315aa0c87b
598403 <matthewforjan@C02T50LJH03Q.local>
2018-08-03 15:20:29 -0400

    put form into separate component for testing purposes

--- src/components/FormDialog.js
+++ src/components/FormDialog.js
@@ -12,7 +12,71 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const FormDialog = ({open, toggleDialog, toggleCheck, addTime, checked}) => {
+export const Form = ({toggleCheck, toggleDialog, checked, addTime, handleCancel}) => {
+  return (
+    <form>
+      <DialogTitle>Enter Time</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin='dense'
+          id='chargeNumber'
+          label='Charge Number'
+          inputRef={x => this.chargeNumber = x}
+          fullWidth
+        />
+        <TextField
+          autoFocus
+          margin='dense'
+          id='location'
+          label='Location'
+          inputRef={x => this.location = x}
+          fullWidth
+        />
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={checked}
+                onChange={toggleCheck}
+                value='checked'
+                color='primary'
+              />
+            }
+            label='Telework?'
+          />
+        </FormGroup>
+      </DialogContent>
+      <DialogActions>
+        <Button 
+          onClick={handleCancel}
+          color='primary'
+          className='cancel'
+        >
+          Cancel
+        </Button>
+        <Button type='submit' className='submit' onClick={(e) => {
+          e.preventDefault();
+          if (!this.chargeNumber.value || !this.location.value) {
+            alert('Please enter a charge number and location');
+            return;
+          }
+          addTime(this.chargeNumber.value, this.location.value, checked);
+          if (checked) toggleCheck();
+          toggleDialog();
+        }}>
+          Enter
+        </Button>
+      </DialogActions>
+    </form>
+  );
+};
+
+export const FormDialog = ({open, toggleDialog, toggleCheck, addTime, checked}) => {
+  const handleCancel = () => {
+    if (checked) toggleCheck();
+    toggleDialog();
+  };
   return (
     <div>
       <Button 
@@ -28,59 +92,13 @@ const FormDialog = ({open, toggleDialog, toggleCheck, addTime, checked}) => {
         open={open}
         onClose={toggleDialog}
       >
-        <form>
-          <DialogTitle>Enter Time</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin='dense'
-              id='chargeNumber'
-              label='Charge Number'
-              inputRef={x => this.chargeNumber = x}
-              fullWidth
-            />
-            <TextField
-              autoFocus
-              margin='dense'
-              id='location'
-              label='Location'
-              inputRef={x => this.location = x}
-              fullWidth
-            />
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={checked}
-                    onChange={toggleCheck}
-                    value='checked'
-                    color='primary'
-                  />
-                }
-                label='Telework?'
-              />
-            </FormGroup>
-          </DialogContent>
-          <DialogActions>
-            <Button 
-              onClick={() => {
-                toggleCheck();
-                toggleDialog();
-              }}
-              color='primary'
-            >
-              Cancel
-            </Button>
-            <Button type='submit' onClick={(e) => {
-              e.preventDefault();
-              addTime(this.chargeNumber.value, this.location.value, checked);
-              toggleCheck();
-              toggleDialog();
-            }}>
-              Enter
-            </Button>
-          </DialogActions>
-        </form>
+        <Form 
+          addTime={addTime} 
+          toggleCheck={toggleCheck}
+          checked={checked} 
+          toggleDialog={toggleDialog}
+          handleCancel={handleCancel}
+        />
       </Dialog>
     </div>
   );
@@ -91,6 +109,14 @@ FormDialog.propTypes = {
   toggleDialog: PropTypes.func,
   toggleCheck: PropTypes.func,
   addTime: PropTypes.func,
+  checked: PropTypes.bool,
+};
+
+Form.propTypes = {
+  toggleDialog: PropTypes.func,
+  toggleCheck: PropTypes.func,
+  addTime: PropTypes.func,
+  handleCancel: PropTypes.func,
   checked: PropTypes.bool,
 };
 
