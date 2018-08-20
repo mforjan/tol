96934a2eed3eaaff0dd88972ea4251e0642dc04a
598403 <matthewforjan@c02t50ljh03q.users.bah.com>
2018-08-20 11:22:59 -0400

    removed unnecessary comment

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import { Provider } from 'react-redux';
 import Main from './components/Main';
 import store from './store';
 
-// configureStoreAsync().then(store => {
 ReactDOM.render(<Provider store={store}>
   <Main />
 </Provider>, document.getElementById('root'));
 registerServiceWorker();
-// });
-
