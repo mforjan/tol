4fc314de35c0f76cd4605430997500c2b388e587
598403 <matthewforjan@c02t50ljh03q.users.bah.com>
2018-08-21 16:49:35 -0400

    added errors reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import timeReport from './timeReport';
 import messages from './messages';
 import absences from './absences';
 import dialogs from './dialogs';
+import errors from './errors';
 
 const rootReducer = combineReducers({
   timeReport,
   absences,
   messages,
   dialogs,
+  errors,
 });
 
 export default rootReducer;
